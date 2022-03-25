6be89b071ac365593045bd6d9467cd48ac3c81f0
dunmininu <oluwaseyi.alaka@torilo.co.uk>
2022-03-25 12:53:21 +0100

    reset password

--- CRM/urls.py
+++ CRM/urls.py
@@ -3,7 +3,14 @@ from django.urls import path
 from django.urls.conf import include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
 from leads.views import LandingPageView, SignUpView
 
@@ -14,6 +21,10 @@ urlpatterns = [
     path("leads/", include("leads.urls", namespace="leads")),
     path("agents/", include("agents.urls", namespace="agents")),
     path("login/", LoginView.as_view(), name="login"),
-    path("signup/", SignUpView.as_view(), name="signup"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("reset-password/", PasswordResetView.as_view(), name="reset-password"),
+    path("password-reset-done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("password-reset-confirm/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("password-reset-complete/", PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
